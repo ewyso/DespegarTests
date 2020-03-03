14f2cd5c81fc3b9b73be287b0f573202e1ded77a
wyso <eric.wysoczanski@gmail.com>
2020-03-03 08:40:48 -0300

    Test Case 2

--- src/test/java/commons/Constants.java
+++ src/test/java/commons/Constants.java
@@ -4,28 +4,40 @@ public class Constants {
 
     public static final String URL_DESPEGAR = "https://www.despegar.com.ar/";
 
+    //Flight select
     public static final String XPATH_ORIGEN = "//div[@class='sbox-3-input -md sbox-3-validation -top-right -icon-left sbox-origin-container places-inline sbox-bind-error-flight-roundtrip-origin-empty']//div[@class='input-container']//input";
     public static final String XPATH_DESTINO = "//div[@class='sbox-3-input -md sbox-3-validation -top-right -icon-left sbox-destination-container sbox-bind-error-flight-roundtrip-destination-empty sbox-bind-error-flight-roundtrip-equal-destination']//input";
     public static final String XPATH_DIV_FECHA_IDA = "//body[@product='flights']//div[@class='input-container sbox-bind-event-click-start-date']//input";
-    public static final String XPATH_DIV_FECHA_VUELTA = ".//input[@class='input-tag sbox-checkout-date']";
     public static final String XPATH_BTN_BUSCAR_VUELO = "//div[@class='sbox-button -ml3-l -mt5-l']//div[@class='sbox-button-container']";
     public static final String XPATH_DIV_DATE_PICKER_ORIGEN = ".//div[@class='datepicker-flights-main']//div[@class='_dpmg2--wrapper _dpmg2--roundtrip _dpmg2--show-info _dpmg2--show']";
-    public static final String xPath_DivDatePicker_Destino = ".//div[@class='_dpmg2--wrapper _dpmg2--roundtrip _dpmg2--show-info _dpmg2--show _dpmg2--transition-displacement]";
     public static final String XPATH_BTN_NEXT = ".//body//div[@class='datepicker-flights-main']//div[@class='_dpmg2--controls-next']";
     public static final String XPATH_DIV_CIUDADES = "//body[@product='flights']//div[@class='ac-wrapper -desktop -show']//div[@class='ac-container']//div[@class='ac-group-container']";
     public static final String XPATH_CALENDAR_PICKER = "div[@class='datepicker-flights-main']//div[contains(@class, '_dpmg2--month _dpmg2--o-')]//div[@class='_dpmg2--month-title']";
 
     public static final String XPATH_BTN_FLIGHT = "//a[contains(@class,'shifu-3-button-circle FLIGHTS paint-circle')]";
     public static final String XPATH_LISTA_CIUDADES = "//body[@product='flights']//div[@class='ac-wrapper -desktop -show']//div[@class='ac-container']//div[@class='ac-group-container']//ul";
-
-    public final String nextMont = ".//div[@class='_dpmg2--controls-next']";
-    public final String prevMont = ".//div[@class='_dpmg2--controls-prev']";
-
     public static final String XPATH_MAIN_DIV_FLIGHTS = "//div[@class='datepicker-flights-main']";
 
-    public static final String XPATH_FLIGHT_PRICES_OPPORTUNITY = "//div[@class='main-content -show']//div[@id='clusters']//div[@class='eva-3-cluster-basic -eva-3-shadow-line-hover']";
-    public static final String XPATH_FLIGHT_FINAL_PRICE = "//body[contains(@class,'show-phone')]/div[@id='flights-container-wrapper']//div[@class='main-content -show']//div[@id='clusters']//div[@class='eva-3-cluster-basic -eva-3-shadow-line-hover']//p[@class='item-fare fare-price']//span[@class='amount price-amount']";
-            //"//div[@class='main-content -show']//div[@id='clusters']//div[@class='eva-3-cluster-basic -eva-3-shadow-line-hover']//p[@class='item-fare fare-price']//span[@class='amount price-amount']";
+    //Flight selection
+    public static final String XPATH_DIV_COMPARATIVA_PRECIOS = "//div[@class='results-cluster-container -skeleton -show']//div[@class='tabs-container']";
+    public static final String XPATH_DIV_FOOTER = "//body//div[@class='eva-3-row footer-links-container -fluid -no-gutter']";
+    public static final String XPATH_SPAN_PRECIO_FINAL = "//div[@id='flights-container']//div[@class='ux-flights-results']//div[@class='col -lg-9 -md-12 -sm-12 results-content-container']//div[@class='main-content -show']//items//div[@class='cluster-container COMMON']//div[@class='cluster-pricebox-container CLUSTER']//div[@class='fare-detail-items']//item-fare[@classes='fare-price']//span[@class='price-wrapper']//span[@class='amount price-amount']";
+    public static final String XPATH_DIV_OPCION_VUELO = ".//body//div[@id='clusters']//div[@class='eva-3-cluster-basic -eva-3-shadow-line-hover']";
+    public static final String XPATH_BOTON_COMPRA = "//div//span[@class='fare-wrapper -eva-3-p-md']//span[contains(text(), *bestPrice*)]/ancestor::*[@class='cluster-pricebox-container CLUSTER']//buy-button";
 
+    //CheckOut
+    public static final String XPATH_DIV_PASAJEROS = "//div[@class='eva-3-card -eva-3-shadow-line travelers frame']";
+    public static final String XPATH_DIV_METODO_PAGO = "//div[@class='eva-3-card -eva-3-shadow-line frame payment-method with-legal-conditions']";
+    public static final String XPATH_DIV_FACTURA = "//div[@class='eva-3-card -eva-3-shadow-line invoice-container frame']//div[@class='invoice-component']";
+    public static final String XPATH_DIV_INFO_CONTACTO = "//div[@class='eva-3-card -eva-3-shadow-line frame contact-info ng-touched ng-pristine ng-invalid']";
+    public static final String XPATH_DIV_TARJETA_CREDITO = "//div[@class='card-slot']//div[@class='header -eva-3-p-md']";
+    public static final String XPATH_DIV_BANCOS_TARJETA = "//div[@class='payment-option-selector-wrap']";
+    public static final String XPATH_BANCO_SANTANDER = "//h5[contains(text(),'Santander')]";
+    public static final String XPATH_TARJETA_VISA = "//h5[contains(text(),'Visa')]";
+    public static final String XPATH_UNA_CUOTA = "//span[contains(text(),'1 pago')]";
+
+    //Progress
     public static final String XPATH_FLIGHT_PROGRESS_BAR = "//div[@class='header-container']//div[@class='progress-bar-background']//div[@class='progress-bar-content']";
+    public static final String XPATH_DIV_SPINNER = "//div[@class='loader-container']//div[@class='loader-spinner']";
+    public static final String XPATH_DIV_MODAL_LOADER = "div[@class='loader-modal-content']";
 }
--- src/test/java/org/selenide/examples/SearchFlightAndBuy.java
+++ src/test/java/org/selenide/examples/SearchFlightAndBuy.java
@@ -1,5 +1,6 @@
 package org.selenide.examples;
 
+import com.codeborne.selenide.Selenide;
 import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 import commons.Constants;
 import commons.FlightsCommons;
@@ -13,8 +14,7 @@ import java.text.ParseException;
 import java.util.List;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class SearchFlightAndBuy extends FlightsCommons {
@@ -81,12 +81,39 @@ public class SearchFlightAndBuy extends FlightsCommons {
         $("title").shouldHave(attribute("text", TITLE_FLIGHT_SEARCH));
 
         //Wait until web loads
-       // $(By.xpath(Constants.XPATH_FLIGHT_PRICES_OPPORTUNITY)).waitUntil(appear, 30000);
         $(By.xpath(Constants.XPATH_FLIGHT_PROGRESS_BAR)).waitUntil(disappear, 30000);
-       // $(By.xpath(Constants.XPATH_FLIGHT_FINAL_PRICE)).waitUntil(appear, 30000);
 
         selectMostExpensiveFlight();
 
+        //Select payment method
+        selectPaymentMethod();
+        //validate CheckOutPage
+        validateCheckOutPage();
+
+
+    }
+
+    private void selectPaymentMethod() {
+        //select card
+        $(By.xpath(Constants.XPATH_DIV_TARJETA_CREDITO)).click();
+        $(By.xpath(Constants.XPATH_DIV_BANCOS_TARJETA)).should(appear);
+        //this items could be parameterized but they aren't in the requirement :)
+        $(By.xpath(Constants.XPATH_BANCO_SANTANDER)).click();
+        $(By.xpath(Constants.XPATH_TARJETA_VISA)).click();
+        $(By.xpath(Constants.XPATH_UNA_CUOTA)).click();
+        //************************************************************************/
+        $(By.xpath(Constants.XPATH_DIV_BANCOS_TARJETA)).should(disappear);
+    }
+
+    private void validateCheckOutPage() {
+        //Passengers
+        $(By.xpath(Constants.XPATH_DIV_PASAJEROS)).should(visible);
+        //Payment method
+        $(By.xpath(Constants.XPATH_DIV_METODO_PAGO)).should(visible);
+        //Invoice
+        $(By.xpath(Constants.XPATH_DIV_FACTURA)).should(visible);
+        //Contact info
+        $(By.xpath(Constants.XPATH_DIV_INFO_CONTACTO)).should(visible);
     }
 
     @Override
@@ -96,22 +123,29 @@ public class SearchFlightAndBuy extends FlightsCommons {
 
 
     public void selectMostExpensiveFlight(){
-        List<WebElement> allFlightPrices = ($(By.xpath("//*")).findElements(By.xpath(Constants.XPATH_FLIGHT_FINAL_PRICE)));
+        $(By.xpath(Constants.XPATH_DIV_COMPARATIVA_PRECIOS)).waitUntil(visible, TIMEOUT);
+        //Scrolls to bottom to load all the page
+        $(By.xpath(Constants.XPATH_DIV_FOOTER)).scrollIntoView(true);
+        //Waits until spinner dissapear
+        $(By.xpath(Constants.XPATH_DIV_SPINNER)).should(disappear);
+        List <WebElement> allFlightPrices = $(By.xpath(".//*")).findElements(By.xpath(Constants.XPATH_SPAN_PRECIO_FINAL));
+        $(By.xpath(Constants.XPATH_DIV_OPCION_VUELO)).scrollIntoView(true);
 
-        List<WebElement> test = $(By.xpath(".//body//div[@id='clusters']")).findElements(By.xpath(".//div[@class='eva-3-cluster-basic -eva-3-shadow-line-hover']"));
         BigDecimal bestPrice = new BigDecimal(0);
         for(int i=0; i < allFlightPrices.size(); i++){
+           // List <WebElement> test = $(By.xpath(".//*")).findElements(By.xpath("//div[@id='flights-container']//div[@class='ux-flights-results']//div[@class='col -lg-9 -md-12 -sm-12 results-content-container']//div[@class='main-content -show']//items//div[@class='cluster-container COMMON']//div[@class='cluster-pricebox-container CLUSTER']//div[@class='fare-detail-items']//item-fare[@classes='fare-price']//span[@class='price-wrapper']//span[@class='amount price-amount']"));
             WebElement flightPrice = allFlightPrices.get(i);
-
             BigDecimal currentPrice = new BigDecimal(flightPrice.getText());
-            //long value = bd.longValue();
             if(currentPrice.compareTo(bestPrice) > 0){
                 bestPrice = currentPrice;
             }
         }
-
         System.out.println("Mejor Precio " + bestPrice);
+        //Click most expensive flight
+        $(By.xpath(Constants.XPATH_BOTON_COMPRA.replace("*bestPrice*", String.valueOf(bestPrice)))).scrollIntoView(true).click();
+        $(By.xpath(Constants.XPATH_DIV_MODAL_LOADER)).should(disappear);
     }
+
 
 
 
